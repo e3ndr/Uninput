ef87c59a6b1fdde61601169d71e7efc87b048c5a
Ender <33337309+e3ndr@users.noreply.github.com>
2022-07-30 07:24:17 -0500

    The events need constructors.

--- src/main/java/xyz/e3ndr/uninput/events/UMouseMoveEvent.java
+++ src/main/java/xyz/e3ndr/uninput/events/UMouseMoveEvent.java
@@ -3,8 +3,10 @@ package xyz.e3ndr.uninput.events;
 import co.casterlabs.rakurai.json.annotating.JsonClass;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 @JsonClass(exposeAll = true)
 public class UMouseMoveEvent extends UEvent {
--- src/main/java/xyz/e3ndr/uninput/events/USpawnEvent.java
+++ src/main/java/xyz/e3ndr/uninput/events/USpawnEvent.java
@@ -3,9 +3,11 @@ package xyz.e3ndr.uninput.events;
 import co.casterlabs.rakurai.json.annotating.JsonClass;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import xyz.e3ndr.uninput.Border;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 @JsonClass(exposeAll = true)
 public class USpawnEvent extends UEvent {
